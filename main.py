fc6c8df244b9bd516e410a3ffea24e9a3332e958
NickChapman1 <66032227+NickChapman1@users.noreply.github.com>
2025-05-15 18:43:25 -0600

    Add files via upload
    First build of the question game, made it so it can be randomized as well

---
+++ main.py
@@ -0,0 +1,23 @@
+##make a question class that has text and answer as attributes. Needs to initalize each time. Then
+##constructor code will take those pieces of data and add them. use init method to initalize two attributs
+from quiz_brain import QuizBrain
+from data import question_data
+from question_model import Question
+import random
+
+question_bank = []
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Question(question_text,question_answer) #call to question function in question_model
+    question_bank.append(new_question)
+
+#random.shuffle(question_bank)  #This makes it random!
+Quizbrain_constructor = QuizBrain(question_bank)
+
+
+while Quizbrain_constructor.is_still_question():
+    Quizbrain_constructor.next_question()
+
+#bring up a question and ask user to answer it.
+# For all quiz functionality add in quiz brain
---
+++ quiz_brain.py
@@ -0,0 +1,50 @@
+##make this ask the questions
+#Check if the answer was correct
+#chcek if were at the end of the quiz.
+#class will have two attributes question number default 0 and question list. will pass over the bank into the list
+#Question number should keep track of which question the user is on to go through the list
+#Method as well called next_question() ,in final veriosn it does display the question number.
+#from main import question_bank
+from tabnanny import check
+import random
+
+#question_list = question_bank
+
+class QuizBrain:
+
+    def __init__(self, q_list):
+        self.question_number = 0
+        self.number_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+        self.random_number = random.choice(self.number_list)
+        self.random_question = ""
+        self.question_list = q_list
+        self.score = 0
+
+    def is_still_question(self):
+       ## test = self.question_number < len(self.question_list)
+        ##print(f"{test}")
+        return self.question_number < len(self.question_list)
+
+    def next_question(self):
+        #set current question equal to the question list indexed to the question num all self
+        current_question = self.question_list[self.question_number]
+        #increment question num
+        self.question_number += 1
+    # self.random_question = random.choice(self.question_list[self.random_number])
+       # print(f"{self.random_question}")
+        #set user answer to input
+        user_answer = input(f"Current score {self.score}...you're on question #{self.question_number}..\n"
+                            f" \n{current_question.question} Is this true or false??").lower()
+        #call to the self check answer funtion and pass the user answer and current question.answer]
+        self.check_answer(current_question.answer, user_answer)
+        return
+
+    def check_answer(self, answer, user_answer):
+        if answer.lower() != user_answer.lower():
+            print(f"\nYou we're incorrect fool. You've now lost. You had a score of {self.score}\n")
+            exit()
+
+        else:
+            self.score += 1
+            print(f"\nBravo yous right! Well done, that puts your score at {self.score}")
+            self.next_question()

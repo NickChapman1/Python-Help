1b3c4fe37ee16c8b52c3fdef9f2ad82a2f3cf3ad
NickChapman1 <66032227+NickChapman1@users.noreply.github.com>
2025-10-22 15:39:48 -0700

    Day 34 My Quiz Api attempt non edited
    You'll need to add some photos and folder structure or rewrite some of the python for the buttons of the ui.

--- ui.py
+++ ui.py
@@ -1,52 +1,67 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
-FONT_NAME = "Courier"
-BACKGROUND_COLOR = "#B1DDC6"
-
 
 class QuizInterface:
-
-    def __init__(self, quiz_brain):
-    #Make all buttons and layouts of the UI
+    #The QuizBrain makes it a required parameter.
+    def __init__(self, quiz_brain: QuizBrain) -> object:
         self.quiz = quiz_brain
         self.window = Tk()
-        self.window.title("Quizler")
-        self.window.config(pady=20, padx=20)
-        #Set up a smaller box on the rectangle. You do self. canvas to add a canvas.
-        self.canvas = Canvas(width=400, height=300, bg=BACKGROUND_COLOR, highlightthickness=0)
-        self.canvas.grid(row=0, column=0, columnspan=2)
+        self.window.title("Quizzler")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+    #Self. makes it a property that can be refrenced anywhere in the class.
+        self.score_label = Label(text="Score: 0", fg = "white", bg=THEME_COLOR)
+        self.score_label.grid(row=0, column = 1)
 
-        # Draw a smaller square
-        self.canvas.create_rectangle(50, 50, 300, 225, fill="white", outline="black")
-        #Add text
-        self.question_text = self.canvas.create_text(175, 140, width=250, text="", font=(FONT_NAME, 12, "italic"), fill="black")
+        self.canvas = Canvas(width=300, height=250, bg="white")
+        #Doing half of the canvas width for the qt.
+        self.question_text = self.canvas.create_text(
+            150,
+            125,
+            width = 280,
+            text="Question text",
+            fill=THEME_COLOR,
+            font=("Arial", 20, "italic")
+        )
 
-    # buttons: All these need to be created outside of the functions so they don't die with them.
-        know_it_pic = PhotoImage(file="images/true.png")
-        dont_know_pic = PhotoImage(file="images/false.png")
-        check_but = Button(image=know_it_pic, highlightthickness=0, command=self.true_pressed)
-        check_but.grid(column=0, row=1)
-        x_but = Button(image=dont_know_pic, highlightthickness=0, command=self.false_pressed)
-        x_but.grid(column=1, row=1)
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
 
-        #Keeps program running. Never ending while loop for the GUI, will get confused if another while loop is near it.
+        true_image = PhotoImage(file="images/true.png")
+        self.true_button = Button(image=true_image, highlightthickness=0, command=self.true_pressed)
+        self.true_button.grid(row=2, column=0)
+
+        false_image = PhotoImage(file="images/false.png")
+        self.false_button = Button(image=false_image, highlightthickness=0, command=self.false_pressed)
+        self.false_button.grid(row=2, column=1)
+
         self.get_next_question()
+
         self.window.mainloop()
 
     def get_next_question(self):
+        self.canvas.config(bg="white")
         if self.quiz.still_has_questions():
+            self.score_label.config(text=f"Score: {self.quiz.score}")
             q_text = self.quiz.next_question()
             self.canvas.itemconfig(self.question_text, text=q_text)
+            # use for testing: print(f"Question being displayed: {q_text}")
         else:
-            self.canvas.itemconfig(self.question_text, text="You've completed the quiz!")
-
-
+            self.canvas.itemconfig(self.question_text, text="You've hit the end of the Quiz!")
+            self.true_button.config(state="disabled")
+            self.false_button.config(state="disabled")
     def true_pressed(self):
-        self.quiz.check_answer("True")
-        self.get_next_question()
-
+        self.give_feedback(self.quiz.check_answer("True"))
 
     def false_pressed(self):
-        self.quiz.check_answer("False")
-        self.get_next_question()
+        is_right = self.quiz.check_answer("False")
+        self.give_feedback(is_right)
+
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
+        #Can't use sleep because the mainloop is running. After is in miliseconds. If you
+        # use a function as an input no parenthesis in the call
+        self.window.after(1000, self.get_next_question)
